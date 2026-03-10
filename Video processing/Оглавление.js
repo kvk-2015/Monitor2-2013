e4a4295f479c087cfc70cb9c49366923d7f00c12
Konstantin Kulakov <16433297+kvk-2015@users.noreply.github.com>
2026-03-10 13:57:30 +0500

    Update Оглавление.js

--- Video processing/Оглавление.js
+++ Video processing/Оглавление.js
@@ -1,6 +1,7 @@
 var str, WshShell = new ActiveXObject("WScript.Shell");
 var isArial = false, textAreaLength = (isArial ? 57: 157), headLength = 11; // isArial - нужно ли печатать на принтере крупно или только смотреть в Far Manager
-var ffProbe = WshShell.ExpandEnvironmentStrings("%FFmpegPath%") + "ffprobe.exe"; tailLength = textAreaLength - headLength; MINIMAL_WORD_LENGTH_AT_END_OF_LINE = 3;
+var FFmpegPath = "%FFmpegPath%", ffProbe = (WshShell.ExpandEnvironmentStrings(FFmpegPath) + "ffprobe.exe").replace(FFmpegPath, "");
+var tailLength = textAreaLength - headLength, MINIMAL_WORD_LENGTH_AT_END_OF_LINE = 3;
 var re = new RegExp("\\s*(.{0," + (tailLength - MINIMAL_WORD_LENGTH_AT_END_OF_LINE) + "}\\S{" + MINIMAL_WORD_LENGTH_AT_END_OF_LINE + "})(?=\\s+|$)","g");
 var fso = new ActiveXObject("Scripting.FileSystemObject"), shellApp = new ActiveXObject("Shell.Application"), header;
 var s, sum, Arg, Args, startFolders = [], dd = (new Array(isArial ? 91 : textAreaLength + 1)).join("-"), fsp = (new Array(isArial ? 16 : headLength - 1)).join(" ");
